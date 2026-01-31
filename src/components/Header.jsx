6174a988dfa2644bcab98f3501c892709f1b6f1a
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-01-31 08:54:22 +0800

    feat: add background and Header CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
+import '../styles/Header.css';
+
 export default function Header() {
   return (
     <header>
-      <h1>Harry Potter</h1>
-      <h2>
-        and the
-        <br />
-        Memory Card Game
-      </h2>
+      <div className="container">
+        <h1>Harry Potter</h1>
+        <h2>
+          and the
+          <br />
+          Memory Card Game
+        </h2>
+      </div>
     </header>
   );
 }
