c10ceb5678e4d1046b15c04745dfb2d692b81efa
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-04-13 09:16:42 +0800

    feat: add loading animation

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,18 +2,22 @@ import { useState, useEffect } from 'react';
 
 export default function useData(url) {
   const [data, setData] = useState(null);
+
   useEffect(() => {
-    let ignore = false;
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        if (!ignore) {
-          setData(json);
-        }
-      });
-    return () => {
-      ignore = true;
-    };
+    setTimeout(() => {
+      let ignore = false;
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          if (!ignore) {
+            setData(json);
+          }
+        });
+      return () => {
+        ignore = true;
+      };
+    }, 1000);
   }, [url]);
+
   return data;
 }
