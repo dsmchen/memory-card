a90e7bbb5230dca24de85cd8f1b59be8c0282ac8
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-04-13 21:42:44 +0800

    fix: remove setTimeout

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -4,19 +4,17 @@ export default function useData(url) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      let ignore = false;
-      fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
-          if (!ignore) {
-            setData(json);
-          }
-        });
-      return () => {
-        ignore = true;
-      };
-    }, 1000);
+    let ignore = false;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) {
+          setData(json);
+        }
+      });
+    return () => {
+      ignore = true;
+    };
   }, [url]);
 
   return data;
